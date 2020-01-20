9cc9cad73bec19e9cbc34d78ed3acd0eb50ce593
Jeremy Philipson <jeremyphilipson@gmail.com>
2020-01-20 12:21:16 -0500

    chore: add single test

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,8 @@
-import useSWR from 'swr'
-import { fetcher, weeklyGamesRoute } from '../utils'
+// import useSWR from 'swr'
+// import { fetcher, weeklyGamesRoute } from '../utils'
 
 const Home = () => {
-  const { data, error } = useSWR(
-    weeklyGamesRoute({
-      seasonYear: '2019',
-      seasonType: 'regular',
-      weekNumber: 4
-    }),
-    fetcher
-  )
-  if (!data) {
-    return null
-  }
-  console.log(data)
-  return <h2>{`Hello ${data.name}`}</h2>
+  return <h2>Hello</h2>
 }
 
 export default Home
